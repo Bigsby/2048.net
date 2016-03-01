7b930386ecb5fdaed05e740aed5e16a5e571e860
Bruno Bernardo <bbernardo@oneviewhealthcare.com>
2016-03-01 23:43:22 +0000

    Add gesture recognition

--- DCCC.XF/DCCC.XF/App.cs
+++ DCCC.XF/DCCC.XF/App.cs
@@ -1,29 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Xamarin.Forms;
 
 namespace DCCC.XF
 {
     public class App : Application
     {
+        private readonly Label _label;
+
         public App()
         {
+            _label = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = "Bigsby in XForms"
+            };
+
+            var panRecognizer = new PanGestureRecognizer();
+            double totalX = 0;
+            double totalY = 0;
+
+            panRecognizer.PanUpdated += (s, e) =>
+            {
+                switch (e.StatusType)
+                {
+                    case GestureStatus.Canceled:
+                    case GestureStatus.Started:
+                        totalX = totalY = 0;
+                        break;
+                    case GestureStatus.Running:
+                        totalX += e.TotalX;
+                        totalY += e.TotalY;
+                        break;
+                    case GestureStatus.Completed:
+                        if (totalX == 0 && totalY == 0) return;
+
+                        if (Math.Abs(totalX) > Math.Abs(totalY))
+                            _label.Text = totalX > 0 ? "Swiped Right!" : "Swiped Left!";
+                        else
+                            _label.Text = totalY > 0 ? "Swiped Down!" : "Swiped Up!";
+
+                        break;
+                }
+            };
+
+            var stackLayout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Fill,
+                Children = {
+                        _label
+                    },
+                BackgroundColor = Color.Pink,
+
+            };
+
+            stackLayout.GestureRecognizers.Add(panRecognizer);
             // The root page of your application
             MainPage = new ContentPage
             {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Bigsby in XForms"
-                        }
-                    }
-                }
+
+                Content = stackLayout,
+                BackgroundColor = Color.Yellow
             };
         }
 
